29289ba471ef7cf3e674d46ade2a064d7910bca8
paulzay <paulotieno2@gmail.com>
2020-12-12 17:57:20 +0300

    write actions for getting shows, one show and filtering shows

---
+++ src/redux/actions/index.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+
+const url = `http://api.tvmaze.com/`
+export function getShows() {
+  return (dispatch) => {
+    axios.get(`${url}shows`, { mode: 'cors' })
+      .then((res) => {
+        let shows = res.data
+        dispatch({ type: 'GET_SHOWS', shows })
+        console.log('triggered')
+      }).catch((err) => {
+        console.log(err)
+      })
+  }
+}
+
+export function getShow(id) {
+  return (dispatch) => {
+    axios.get(`${url}/shows/${id}`, { mode: 'cors' })
+      .then((res) => {
+        let show = res.data
+        dispatch({ type: 'VIEW_SHOW', show })
+      }).catch((err) => console.log(err))
+  }
+}
+
+export const changeFilter = network => ({
+  type: 'CHANGE_FILTER',
+  network,
+});
