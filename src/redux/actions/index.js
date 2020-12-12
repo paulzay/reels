0f64c9de825da247bb9d33692fe448abdba6e6cb
paulzay <paulotieno2@gmail.com>
2020-12-13 00:13:26 +0300

    fix url security issue

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://api.tvmaze.com/';
+const url = 'https://api.tvmaze.com/';
 export function getShows() {
   return dispatch => {
     axios.get(`${url}shows?page1`, { mode: 'cors' })
@@ -13,7 +13,7 @@ export function getShows() {
 
 export function getShow(id) {
   return dispatch => {
-    axios.get(`http://api.tvmaze.com/shows/${id}`, { mode: 'cors' })
+    axios.get(`https://api.tvmaze.com/shows/${id}`, { mode: 'cors' })
       .then(res => {
         const show = res.data;
         dispatch({ type: 'VIEW_SHOW', show });
