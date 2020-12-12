9c8e1cef3b3222a849d9536b337a93fcdf455c3e
paulzay <paulotieno2@gmail.com>
2020-12-12 18:01:27 +0300

    setup store config

---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+import rootReducer from './reducers';
+
+export default function configureStore() {
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+    return createStore(
+        rootReducer,
+        composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant())),
+    );
+}
