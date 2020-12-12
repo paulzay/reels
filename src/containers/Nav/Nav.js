6123dac337c6b1b62502ed36f006ec20b3a5e58b
paulzay <paulotieno2@gmail.com>
2020-12-12 18:06:13 +0300

    build the navbar

---
+++ src/containers/Nav/Nav.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './nav.css';
+
+export default function Nav() {
+  return (
+    <div className="nav">
+      <img className="nav__logo" src="https://upload.wikimedia.org/wikipedia/commons/5/54/Thenewsroom.jpg" alt="logo" />
+    </div>
+  );
+}
