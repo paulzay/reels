50e812e492b575d35a50dd87bc8f0de1ebdc9b6b
paulzay <paulotieno2@gmail.com>
2020-12-12 18:08:20 +0300

    build filter component and export to shows

---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,31 @@
+import React, { useCallback } from 'react';
+import PropTypes from 'prop-types';
+
+const networks = [
+  'All',
+  'CBS',
+  'CTV Sci-Fi Channel',
+  'CNN',
+  'The CW',
+  'HBO',
+  'Showtime',
+];
+
+export default function NetworkFilter({ changeFilter }) {
+  const handleChange = useCallback(event => {
+    changeFilter(event.target.value);
+  }, [changeFilter]);
+
+  return (
+    <div>
+      <select className="form-control" onChange={handleChange}>
+        {networks.map(network => (
+          <option key={network} value={network}>{network}</option>
+        ))}
+      </select>
+    </div>
+  );
+}
+NetworkFilter.propTypes = {
+  changeFilter: PropTypes.func.isRequired,
+};
