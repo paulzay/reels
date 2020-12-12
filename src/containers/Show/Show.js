9e0f54716010382cd4827362478b7f563362dee6
paulzay <paulotieno2@gmail.com>
2020-12-12 21:34:43 +0300

    finish up showing details of single show

--- src/containers/Show/Show.js
+++ src/containers/Show/Show.js
@@ -17,15 +17,21 @@ class Show extends Component {
         const { name, type, language, image, rating, summary } = this.props._show
 
         return (
-        <>
-            <div></div>
-            <p>{name}</p>
-        </>
+        <div className="show">
+            <h3 className="title">{name}</h3>
+            <div className="body">
+        <p>Language:{language}</p>
+        <p>{type}</p>
+        <p className=""dangerouslySetInnerHTML={{__html: summary}}>
+        </p>
+                <p></p>
+            </div>
+        </div>
         )
     }
 }
 Show.propTypes = {
-    params: PropTypes.object.isRequired
+    params: PropTypes.object,
 }
 
 const mapStateToProps = state => {
