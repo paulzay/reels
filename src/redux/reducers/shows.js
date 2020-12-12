79d688cd5ed6777f423d00a87161a00a19a963ad
paulzay <paulotieno2@gmail.com>
2020-12-12 17:59:28 +0300

    build the shows reducer that will get shows, one show or return state

---
+++ src/redux/reducers/shows.js
@@ -0,0 +1,21 @@
+const initialState = {
+    shows: [],
+    show: {}
+}
+
+export default (state=initialState, action) =>{
+    switch(action.type){
+        case 'GET_SHOWS' :
+            return {
+                ...state,
+                shows: action.shows
+            }
+        case 'VIEW_SHOW':
+            return {
+                ...state,
+                show: action.show
+            }
+        default:
+            return state
+    }
+}
