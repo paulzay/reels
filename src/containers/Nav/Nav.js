2fbb196d1e605838fc5de819a90dc764138798a1
paulzay <paulotieno2@gmail.com>
2020-12-13 18:36:18 +0300

    work on navbar

--- src/containers/Nav/Nav.js
+++ src/containers/Nav/Nav.js
@@ -5,6 +5,7 @@ export default function Nav() {
   return (
     <div className="nav">
       <img className="nav__logo" src="https://upload.wikimedia.org/wikipedia/commons/4/46/Movie-reel.svg" alt="logo" />
+      <p>REELS</p>
     </div>
   );
 }
