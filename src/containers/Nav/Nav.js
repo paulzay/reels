ea7dc1750c9f4a3c0624bedc30275493fe0fc967
paulzay <paulotieno2@gmail.com>
2020-12-14 15:32:23 +0300

    add link to nav

--- src/containers/Nav/Nav.js
+++ src/containers/Nav/Nav.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
 import './nav.css';
 
 export default function Nav() {
   return (
     <div className="nav">
       <img className="nav__logo" src="https://upload.wikimedia.org/wikipedia/commons/4/46/Movie-reel.svg" alt="logo" />
-      <p>REELS</p>
+
+      <NavLink to="/" exact>
+        <p>REELS</p>
+      </NavLink>
     </div>
   );
 }
